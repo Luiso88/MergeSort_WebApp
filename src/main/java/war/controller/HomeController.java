0776732bf5e83c7f88141ad891c5822e38d5ba84
Luis Castillo <lcastillo770@gmail.com>
2020-05-02 00:09:27 +0200

    I have updated the pom and homecontroller files.

--- src/main/java/war/controller/HomeController.java
+++ src/main/java/war/controller/HomeController.java
@@ -1,13 +1,10 @@
 package war.controller;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import Sorting.MergeSortClass;
 
 @Controller
 public class HomeController {
@@ -19,6 +16,11 @@ public class HomeController {
 
 	@RequestMapping(value = "/")
 	public ModelAndView receiveArrayOfValues(@RequestParam int[] values) {
+		for (int i = 0; i < values.length; i++) {
+			System.out.println(values[i]);
+		}
+		MergeSortClass m1 = new MergeSortClass();
+		m1.mergeSort(values, values.length);
 		for (int i = 0; i < values.length; i++) {
 			System.out.println(values[i]);
 		}
